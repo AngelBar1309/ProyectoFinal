7498170158bf0de67f5c1b5b621958b49dea00e5
ÁngelBarajasJR <angel_aba_1309@live.com>
2016-10-26 07:38:31 -0700

    creacion del proyecto

---
+++ ProyectoFinalMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProyectoFinalMVC.MvcApplication" Language="C#" %>
---
+++ ProyectoFinalMVC/Models/Clientes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoFinalMVC.Models
+{
+    public class Clientes
+    {
+        public int clienteID { get; set; }
+
+        public string nombre { get; set; }
+
+        public string apellidoP { get; set; }
+        
+        public string apellidoM { get; set; }
+
+        public string direccion { get; set; }
+
+        public int telefono { get; set; }
+
+
+
+    }
+}

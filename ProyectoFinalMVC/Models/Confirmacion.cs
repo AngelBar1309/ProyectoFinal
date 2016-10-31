92a7c0da3c733dffe9306bd19d6278188b473349
oscar2196 <mercury_zzz9@hotmail.com>
2016-10-31 07:41:02 -0600

    Entidades de Confirmacion y Dentistas, listas

--- ProyectoFinalMVC/Models/Confirmacion.cs
+++ ProyectoFinalMVC/Models/Confirmacion.cs
@@ -8,5 +8,23 @@ namespace ProyectoFinalMVC.Models
     public class Confirmacion
     {
 
+        public int confirmacionID { get; set; }
+
+        public int clienteID { get; set; }
+
+        public DateTime fechaIn { get; set; }
+
+        public DateTime fechaOut { get; set; }
+
+        public int citaID { get; set; }
+
+        public double costoMoney { get; set; }
+
+        public bool confirmacion { get; set; }
+
+        public string comentarios { get; set; }
+
+        virtual public Dentistas dentista { get; set; }
+        virtual public Clientes clientes { get; set; }
     }
 }
--- ProyectoFinalMVC/Models/Dentistas.cs
+++ ProyectoFinalMVC/Models/Dentistas.cs
@@ -7,6 +7,15 @@ namespace ProyectoFinalMVC.Models
 {
     public class Dentistas
     {
+        public int dentistasID { get; set; }
+
+        public int nombre { get; set; }
+
+        public string especialidad { get; set; }
+
+        public int telefono { get; set; }
+
+        public string direccion { get; set; }
 
     }
 }
